1474b227158f0cc0052c8043607fc870f59c4133
Prajwal Bashyal <bashyalprajwal24@gmail.com>
2025-11-17 19:24:34 +0545

    add quicksort recursive function

--- Third_sem/DSA/quicksort.c
+++ Third_sem/DSA/quicksort.c
@@ -17,6 +17,12 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 
 }
 
