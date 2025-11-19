ace0c04dd91686e51a20ca5b5b825d042e84f2bd
Prajwal Bashyal <bashyalprajwal24@gmail.com>
2025-11-19 07:49:47 +0545

    program to create a new node [struct datatypes]

---
+++ Third_sem/DSA/struct_3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for the tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+
+int main() {
+    // Create the root and other nodes
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    // Perform in-order traversal
+    printf("In-order Traversal: ");
+    inOrder(root);
+
+    return 0;
+}
